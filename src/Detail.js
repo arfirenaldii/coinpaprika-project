6d2100845c6bf5bd39207a1f1ae81bb962d91fcf
arfi.justika <arfi@justika.com>
2022-08-18 13:33:34 +0700

    add simple layout

--- src/Detail.js
+++ src/Detail.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useParams } from "react-router-dom";
 
 function Detail() {
+  let params = useParams();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Detail
-        </p>
-      </header>
+    <div>
+      <p>Detail</p>
+      <p>{params.id}</p>
     </div>
   );
 }
