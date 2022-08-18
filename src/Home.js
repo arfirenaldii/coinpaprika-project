62538444e45d9e5f36aac3d24135a758b25b71b8
arfi.justika <arfi@justika.com>
2022-08-18 16:17:11 +0700

    add card tablwe

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Table from 'react-bootstrap/Table';
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 import Container from './components/Container';
 import styled from 'styled-components';
@@ -15,6 +16,23 @@ const StyledTable = styled(Table)`
   &>tbody>tr:nth-child(even)>th {
     background-color: #F3F7FB;
   }
+`
+
+const StyledCard = styled(Card)`
+  padding: 18px 30px;
+  box-shadow: 0px 4px 8px rgba(0, 148, 255, 0.25);
+  border-radius: 10px;
+`
+
+const TitleSmall = styled.div`
+  font-size: 12px;
+  color: #ACBCCF;
+  margin: 14px 0px;
+`
+
+const Title = styled.div`
+  font-size: 16px;
+  color: #2569A5;
 `
 
 function Select() {
@@ -35,7 +53,7 @@ function Search() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="search" />
+      <input type="search" placeholder="Search" />
       <Button type="submit">Search</Button>
     </form>
   )
@@ -57,46 +75,49 @@ function Home() {
 
   return (
     <Container>
-      <div>Coin List</div>
-      <Stack direction="horizontal" gap={2} style={{ margin: '38px 0px' }}>
-        <Select />
-        <Search />
-      </Stack>
-      {loading ?
-        <div>loading...</div>
-        :
-        <StyledTable>
-          <thead className='text-white' style={{ backgroundColor: '#3783C6' }}>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Symbol</th>
-              <th>rank</th>
-              <th>Type</th>
-              <th>Active</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((value, index) =>
-              index <= 4 &&
-              <tr key={index}>
-                <td>
-                  <Link to={`/detail/${value.id}`} style={{ color: '#0062A6' }}>{value.id}</Link>
-                </td>
-                <td>{value.name}</td>
-                <td>{value.symbol}</td>
-                <td>{value.rank}</td>
-                <td>{value.type}</td>
-                <td>{value.is_active ? 'True' : 'False'}</td>
-                <td>
-                  <Button style={{ backgroundColor: '#E11730' }}>Delete</Button>
-                </td>
+      <TitleSmall>Coin List</TitleSmall>
+      <StyledCard>
+        <Title>Coin List</Title>
+        <Stack direction="horizontal" gap={2} style={{ margin: '38px 0px' }}>
+          <Select />
+          <Search />
+        </Stack>
+        {loading ?
+          <div>loading...</div>
+          :
+          <StyledTable>
+            <thead className='text-white' style={{ backgroundColor: '#3783C6' }}>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Symbol</th>
+                <th>rank</th>
+                <th>Type</th>
+                <th>Active</th>
+                <th>Action</th>
               </tr>
-            )}
-          </tbody>
-        </StyledTable>
-      }
+            </thead>
+            <tbody>
+              {data.map((value, index) =>
+                index <= 3 &&
+                <tr key={index}>
+                  <td>
+                    <Link to={`/detail/${value.id}`} style={{ color: '#0062A6' }}>{value.id}</Link>
+                  </td>
+                  <td>{value.name}</td>
+                  <td>{value.symbol}</td>
+                  <td>{value.rank}</td>
+                  <td>{value.type}</td>
+                  <td>{value.is_active ? 'True' : 'False'}</td>
+                  <td>
+                    <Button style={{ backgroundColor: '#E11730' }}>Delete</Button>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </StyledTable>
+        }
+      </StyledCard>
     </Container>
   );
 }
