d3cb397ff17aac7e81ee6f25ad86926c3b5d17b2
Arfi Renaldi <arfirenaldi@gmail.com>
2022-08-19 10:38:19 +0700

    add pagination

--- src/Home.js
+++ src/Home.js
@@ -224,19 +224,6 @@ function Home() {
                 setCurrentPage={setCurrentPage}
                 resultPerPage={resultPerPage}
               />
-              <StyledPagination className="justify-content-end" style={{ marginTop: '25px' }}>
-                <Pagination.Prev />
-                <Pagination.Item active>{1}</Pagination.Item>
-                <Pagination.Item>{2}</Pagination.Item>
-                <Pagination.Item>{3}</Pagination.Item>
-                <Pagination.Item>{4}</Pagination.Item>
-                <Pagination.Item>{5}</Pagination.Item>
-                <Pagination.Item>{6}</Pagination.Item>
-                <Pagination.Item>{7}</Pagination.Item>
-                <Pagination.Item>{8}</Pagination.Item>
-                <Pagination.Item>{9}</Pagination.Item>
-                <Pagination.Next />
-              </StyledPagination>
             </div>
           }
         </StyledCard>
